9ddb3f669b8e92f63fbb8e7e3c797ff757092530
ridhi sharma <ridhisharma2521@gmail.com>
2026-02-28 12:16:18 +0530

    Hyperparameter Tuning

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -21,7 +21,7 @@ from src.utils import evaluate_models
 
 @dataclass
 class ModelTrainerConfig:
-  trained_model_file_path=os.path.join("artifacts","model.pkl")
+  trained_model_file_path=os.path.join("artifact","model.pkl")
 
 
 class ModelTrainer:
